c574bd6aaada3c30d733dd0a8835bb87c2052c1b
robotsch <christianhumble22@gmail.com>
2022-04-20 17:16:34 -0400

    implement header, show, empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-
   return (
-    <article className="appointment">
-      {props.time ? <p>Appointment at {props.time}</p> : <p>No Appointments</p>}
-    </article>
+    <Fragment>
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </Fragment>
   );
 }
