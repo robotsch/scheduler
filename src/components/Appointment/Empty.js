6306ba9e5a0f6853edac56f79674d7f809fabbca
robotsch <christianhumble22@gmail.com>
2022-04-20 14:45:01 -0400

    add Empty component

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -4,7 +4,12 @@ import "./styles.scss";
 export default function Empty(props) {
   return (
     <main className="appointment__add">
-      <img className="appointment__add-button" src="images/add.png" alt="Add" />
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
     </main>
   );
 }
