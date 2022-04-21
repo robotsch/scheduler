6613ff4da10fc5c376664efd662bf9295554a939
robotsch <christianhumble22@gmail.com>
2022-04-21 16:11:01 -0400

    implement state object, remove appointments hardcoded values

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
-import { useState } from "react";
-
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import Appointment from "components/Appointment";
+import axios from "axios";
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {},
+  });
+  const setDay = (day) => {
+    setState({ ...state, day });
+  };
+  const setDays = (days) => {
+    setState((prev) => ({ ...prev, days }));
+  };
+  useEffect(() => {
+    axios.get("/api/days").then((res) => setDays(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios.get("/api/days").then((res) => setDays(res.data));
+  }, []);
+
+  const appointmentArr = [];
+
+  Object.values(appointments).forEach((appointment) => {
+    appointmentArr.push(<Appointment key={appointment.id} {...appointment} />);
+  });
 
   return (
     <main className="layout">
@@ -35,11 +40,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
+          <DayList days={state.days} value={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -47,9 +48,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{appointmentArr}</section>
     </main>
   );
 }
