556f03c084cad853ff216d68802f7abcf9f17a7b
robotsch <christianhumble22@gmail.com>
2022-04-23 11:18:06 -0400

    add selector helper function and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,3 +4,13 @@ export function getAppointmentsForDay(state, day) {
     ? []
     : filteredDays[0].appointments.map((apt) => state.appointments[apt]);
 }
+
+//  
+export function getInterview(state, interview) {
+  return interview
+    ? {
+        student: interview.student,
+        interviewer: { ...state.interviewers[interview.interviewer] },
+      }
+    : null;
+}
