f6ec52322177647c05e0cb5e14a82ee6ebc0c339
robotsch <christianhumble22@gmail.com>
2022-04-23 11:17:50 -0400

    implement selector, refactor to use getInterview selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,32 +3,44 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import axios from "axios";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {},
+    appointments: {},
   });
+
   const setDay = (day) => {
     setState({ ...state, day });
   };
-  const setDays = (days) => {
-    setState((prev) => ({ ...prev, days }));
-  };
-  useEffect(() => {
-    axios.get("/api/days").then((res) => setDays(res.data));
-  }, []);
 
   useEffect(() => {
-    axios.get("/api/days").then((res) => setDays(res.data));
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
 
-  const appointmentArr = [];
-
-  Object.values(appointments).forEach((appointment) => {
-    appointmentArr.push(<Appointment key={appointment.id} {...appointment} />);
-  });
+  const schedule = getAppointmentsForDay(state, state.day).map(
+    (appointment) => (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={getInterview(state, appointment.interview)}
+      />
+    )
+  );
 
   return (
     <main className="layout">
@@ -48,7 +60,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">{appointmentArr}</section>
+      <section className="schedule">{schedule}</section>
     </main>
   );
 }
