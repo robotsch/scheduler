132765695c953b403d482ebf25ee977de1cc92bd
robotsch <christianhumble22@gmail.com>
2022-04-23 12:46:11 -0400

    add useVisualMode custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // Small function for DRYness
+  const retPoppedArr = function (arr) {
+    const newArr = [...arr];
+    newArr.pop();
+    return newArr;
+  };
+
+  // Move to new mode, replace current place in history if replace === true
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    setHistory((prev) => {
+      return replace ? [...retPoppedArr(prev), newMode] : [...prev, newMode];
+    });
+  };
+
+  // Move back to previous mode in history
+  const back = () => {
+    if (history.lenght <= 2) {
+      return;
+    }
+    setMode(history[history.length - 2]);
+    setHistory((prev) => retPoppedArr(prev));
+  };
+  return { mode, transition, back };
+}
