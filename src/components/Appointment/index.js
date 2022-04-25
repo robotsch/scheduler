2f127bf968b5c6eb1348f3d3046be84d2e86d69a
robotsch <christianhumble22@gmail.com>
2022-04-25 15:50:52 -0400

    add deletion and confirm functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,37 +3,60 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 import Form from "./Form";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const CONFIRM = "CONFIRM";
 
+  // Import and set default visual modes
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  // Visual mode and function call to save new interview
   const save = (studentName, interviewer) => {
+    transition(SAVING);
     const interview = {
       student: studentName,
       interviewer,
     };
-    props.bookInterview(props.id, interview);
-    transition(SHOW);
+    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+  };
+  
+  // Visual mode and function call to delete existing interview
+  const onDelete = (id) => {
+    if (mode !== CONFIRM) {
+      transition(CONFIRM);
+    }
+    if (mode === CONFIRM) {
+      transition(SAVING);
+      props.cancelInterview(id).then(() => transition(EMPTY));
+    }
   };
 
   return (
     <Fragment>
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CONFIRM && (
+        <Confirm id={props.id} onCancel={back} onConfirm={onDelete} />
+      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={onDelete}
         />
       )}
+
+      {mode === SAVING && <Status message="Saving" />}
       {mode === CREATE && (
         <Form interviewers={props.interviewers} onCancel={back} onSave={save} />
       )}
