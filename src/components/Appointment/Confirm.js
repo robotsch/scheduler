72c3e481ce0c71a6701600f4cf5a1bd180eb3c25
robotsch <christianhumble22@gmail.com>
2022-04-25 16:20:35 -0400

    add comment

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "components/Button";
 import "./styles.scss";
 
+// Confirmation component that shows when cancelling an appointment
 export default function Confirm(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
